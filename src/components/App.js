ac7327f2ceac074c6046da523098f2e998e2aab8
csagymbek <csagymbek@gmail.com>
2020-11-13 23:10:21 -0800

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "../styles/App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      {/* Header  */}
+      <Router>
+        <Header />
+        <Switch>
+          <Route to="/">
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+        {/* Tinder Cards  */}
+        {/* Buttons  */}
+        {/* Chat screen  */}
+        {/* Individual chat screen  */}
+      </Router>
+    </div>
+  );
 }
